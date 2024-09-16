89b70f5a24d3a63bd9479bfbf5051e06449ff090
Nguyen Dong Loi <ndl300803@gmail.com>
2024-09-16 16:12:38 +0700

    up

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretRight } from "@fortawesome/free-solid-svg-icons";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Col, Button } from "react-bootstrap";
 
 // Footer Component
 const Footer = () => {
